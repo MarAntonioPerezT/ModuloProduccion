19d5622b684f603b3b6c6599763e845969caecf3
emreyes2023 <131127824+emreyes2023@users.noreply.github.com>
2023-05-01 15:34:38 -0600

    Creación de excepciones
    Se agregó las excepciones para verificar si hay estados de una orden, procesos, productos y usuarios

---
+++ src/main/java/com/springApplication/moduloProduccion/util/exceptions/ProcesosNotFoundException.java
@@ -0,0 +1,11 @@
+package com.springApplication.moduloProduccion.util.exceptions;
+
+public class ProcesosNotFoundException extends Exception{
+
+    public ProcesosNotFoundException(String mensaje){
+        super(mensaje);
+    }
+
+    public ProcesosNotFoundException(){}
+
+}
---
+++ src/main/java/com/springApplication/moduloProduccion/util/exceptions/ProductosNotFoundException.java
@@ -0,0 +1,15 @@
+package com.springApplication.moduloProduccion.util.exceptions;
+
+public class ProductosNotFoundException extends Exception{
+
+    public ProductosNotFoundException(String mensaje){
+
+        super(mensaje);
+    }
+
+    public ProductosNotFoundException(){
+
+
+    }
+
+}
